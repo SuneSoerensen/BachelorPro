413e55f402dca879a3eb9efcf5f4a76799ded2d9
FredBF <36847271+FredBF@users.noreply.github.com>
2018-03-18 16:17:28 +0100

    Add files via upload

--- TIAFC/test/source.cpp
+++ TIAFC/test/source.cpp
@@ -16,22 +16,41 @@ int main()
 
 		while (run == "y")
 		{
-			cout << "select ''Calib'', ''GraspRegs'' or ''GetCoords'' (c / r / g): ";
+			cout << "select ''Calib'', ''GetCoords'' or ''GraspRegs'' (1, 2, 3): ";
 			cin >> mode;
 
-			if (mode == "c")
+			if (mode == "1")
 			{
 				test.Calib();
 			}
-			if (mode == "g")
+			if (mode == "2")
 			{
 				Coords objCoords = test.GetObjCoords();
 				cout << "real coords: " << objCoords.x << ";" << objCoords.y << endl;
 			}
-			if (mode == "r")
+			if (mode == "3")
 			{
-				test.FindGraspRegs(test.graspRegsList, test.contourImage);
+				test.FindGraspRegs(test.graspRegsList, test.contourImage, test.contourMatrix);
 			}
+
+			/*Coords vecA;
+			cout << "vecA:" << endl;
+			cout << "x = ";
+			cin >> vecA.x;
+			cout << "y = ";
+			cin >> vecA.y;
+			cout << endl;
+
+			Coords vecB;
+			cout << "vecB:" << endl;
+			cout << "x = ";
+			cin >> vecB.x;
+			cout << "y = ";
+			cin >> vecB.y;
+			cout << endl;
+
+			cout << test.CalcAngle(vecA, vecB) << endl;
+			cout << endl;*/
 
 			cout << "Do again?" << endl;
 			cout << "(y / n): ";
