7dc611808f6d2645935567998f975564a024502f
SuneSoerensen <29883744+SuneSoerensen@users.noreply.github.com>
2018-03-20 12:45:25 +0100

    CustGraspHeight test added

---
+++ Tests/CustGraspHeight/CMakeFiles/SDHControl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDHControl.dir/home/sune/BachelorPro/SDHControl/src/SDHControl.cpp.o"
+  "libs/libSDHControl.pdb"
+  "libs/libSDHControl.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDHControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tests/CustGraspHeight/CustGraspHeight.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "SDHControl.hpp"
+#include "URControl.hpp"
+#include <string>
+#include <unistd.h>
+
+
+using namespace std;
+
+int main()
+{
+  string wishToCont;
+
+  //For UR-arm
+  string ip = "192.168.100.4";
+  int port = 30002;
+  double armRelZ;
+
+  //For hand
+  double distA;
+  double distB;
+  double distC;
+  double howMuchLower;
+
+
+
+  cout << "Welcome to CustGrasp.cpp" << endl;
+
+  try
+  {
+    //SDHControl hand("/dev/pcanusb32");
+
+    //URControl ur(ip, port);
+    //ur.connect();
+  }
+  catch(const char e[])
+  {
+    cout << e << endl;
+  }
+
+  cout << "Enter distance for finger A: ";
+  cin >> distA;
+  cout << "Enter distance for finger B: ";
+  cin >> distB;
+  cout << "Enter distance for finger C: ";
+  cin >> distC;
+
+  while(true)
+  {
+    cout << "Moving arm to init" << endl;
+    //ur.moveToInit();
+    cout << "Moving hand to init" << endl;
+    //sdh.goToInit();
+    cout << "Enter a grasping height offset: ";
+    cin >> howMuchLower;
+    cout << "Enter relative z-coordinate for arm: ";
+    cin >> armRelZ;
+
+    try
+    {
+      cout << "Moving arm down" << endl;
+      //ur.moveRel(0.0, 0.0, armRelZ);
+      cout << "Grasping" << endl;
+      //hand.grasp(distA, distB, distC, howMuchLower);
+      cout << "Moving arm up to init + 100mm" << endl;
+      //ur.moveRel(0.0, 0.0, -(armRelZ+100));
+      cout << "Fully stopping hand" << endl;
+      //hand.fullStop();
+    }
+    catch(const char e[])
+    {
+      cout << e << endl;
+    }
+
+    cout << "Do you wish to repeat? (y/n): ";
+    cin >> wishToCont;
+
+    if(wishToCont != "y")
+    {
+      cout << "Terminating" << endl;
+      break;
+    }
+  }
+
+
+  return 0;
+}
