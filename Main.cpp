7cea1b3fc47b7d705689a445d81695d5f07c4b36
SuneSoerensen <sonnysune@msn.com>
2018-04-18 11:01:55 +0200

    Created main

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "SDHControl.hpp"
+#include "URControl.hpp"
+#include "Vision.hpp"
+
+using namespace std;
+
+int main()
+{
+  cout << "Welcome to grasp-utility! Press any key to begin." << endl;
+  cin.get();
+
+  return 0;
+}
